0de382784b5c18cca0a93729b86c2525b0657c28
Dav <davidtorrezreyes@gmail.com>
2020-07-23 08:09:30 -0500

    Agregando portapapeles

--- .scripts/portapapeles.sh
+++ .scripts/portapapeles.sh
@@ -1,1 +1,3 @@
-rofi -modi "clipboard:/home/medicendav/.scripts/mclip.py menu" -show clipboard && /home/medicendav/.scripts/mclip.py paste
+#!/bin/bash
+
+copyq copy $(copyq read 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 | rofi -dmenu -i -p "Portapapeles" -theme-str '#listview {layout: vertical; lines: 12;} horibox {orientation: vertical;}')
